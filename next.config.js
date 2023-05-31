7fa1cb41461d8bd1bc13d655eb683287ef0d6d0b
makoto abe <abe00makoto@gmail.com>
2023-05-31 17:05:51 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    // Target must be serverless
-    // target: 'serverless'
+    images: {
+        domains: ['m0a-mystudy.github.io'], // ここにあなたが使用している画像のドメインをリスト化します
+        unoptimized: true,
+      },
   };
