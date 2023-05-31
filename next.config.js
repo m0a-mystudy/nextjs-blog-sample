2715d5707501ea27ac7ff420b86892423ba3bc0f
makoto abe <abe00makoto@gmail.com>
2023-05-31 17:10:07 +0900

    fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    basePath: '/nextjs-blog-sample',
+    assetPrefix: '/nextjs-blog-sample/',
     images: {
         domains: ['m0a-mystudy.github.io'], // ここにあなたが使用している画像のドメインをリスト化します
         unoptimized: true,
